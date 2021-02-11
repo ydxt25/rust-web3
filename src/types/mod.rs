2155dc13ab52add11083f131ca1f910e713cbe39
yujh <yujh@hardrole.com>
2021-02-11 12:58:08 +0800

    update

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -39,6 +39,6 @@ pub use self::uint::{H128, H160, H2048, H256, H512, H520, H64, U128, U256, U64};
 pub use self::work::Work;
 
 /// Address
-pub type Address = H160;
+pub type Address = String;
 /// Index in block
 pub type Index = U64;
